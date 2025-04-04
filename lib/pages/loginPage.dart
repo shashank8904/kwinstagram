2e47de812e02e37e4d957f9ef7d11510c6c34a43
Shashank Yadav N <shashankyadavn@Shashanks-MacBook-Air.local>
2025-04-04 16:18:47 +0530

    loginpage update

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -8,6 +8,8 @@ class loginPage extends StatefulWidget {
 
 class _loginPageState extends State<loginPage> {
   double? height,width;
+   String? _email;
+   String? _password;
 
   final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
 
@@ -27,9 +29,10 @@ class _loginPageState extends State<loginPage> {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                titlwWidget(),
+                titleWidget(),
                 loginForm(),
                 loginButton(),
+                navigatetoRegisterPage(),
               ],
             ),
           ),),
@@ -37,7 +40,7 @@ class _loginPageState extends State<loginPage> {
       ),
     );
   }
-  Widget titlwWidget(){
+  Widget titleWidget(){
     return Text("Kwinstagram",
     style: TextStyle(
       color: Colors.black,
@@ -56,39 +59,86 @@ class _loginPageState extends State<loginPage> {
         mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-        emailTextfield(),
-          paswwordTextfield()
+        emailTextfield(), paswwordTextfield()
         ],
       )),
     );
   }
 
   Widget emailTextfield(){
-    return TextField();
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Email...",
+        hintStyle: TextStyle(
+          color: Colors.grey
+        )
+      ),
+      onSaved: (_value){
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value){
+       bool _result = _value!.contains(
+         RegExp(
+             r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"),
+        );
+      return _result ? null : "please enter a valid Email";
+      },
+    );
   }
 
   Widget paswwordTextfield(){
-    return TextField();
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Password",
+        hintStyle: TextStyle(
+          color: Colors.grey,
+        ),
+      ),
+      onSaved: (_value){
+        setState(() {
+          _password = _value;
+        });
+      },
+      validator: (_value) => _value!.length > 6 ? null : "Password must carry 6 characters",
+
+    );
   }
-
-
-
 
   Widget loginButton(){
     return MaterialButton(
       color: Colors.redAccent,
       height: height!*0.07,
       minWidth: width!*0.7,
-      onPressed: (){},
+      onPressed: _loginUser,
     child: Text("Login",
       style: TextStyle(
         color: Colors.white,
-        fontSize: width!*0.05,
+        fontSize: width!*0.059,
         fontWeight: FontWeight.w500,
       ),
     ),);
   }
 
+  
+  Widget navigatetoRegisterPage(){
+    return GestureDetector(
+      onTap:()=> Navigator.pushNamed(context, 'register'),
+      child: Text("Don't have account?",
+      style: TextStyle(
+        fontSize: width!*0.04,
+        color: Colors.black,
+        fontWeight: FontWeight.w400,
+      ),),
+    );
+  }
+  
 
-
+  void _loginUser(){
+    if(_loginFormKey.currentState!.validate()){
+    _loginFormKey.currentState!.save();
+    }
+  }
 }
--- lib/pages/registerPage.dart
+++ lib/pages/registerPage.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
 class registerPage extends StatefulWidget {
-  const registerPage({super.key});
+
 
   @override
   State<registerPage> createState() => _registerPageState();
 }
 
 class _registerPageState extends State<registerPage> {
+  double? height,width;
   @override
   Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Container(color: Colors.red,),
+      body: SafeArea(child: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            titleWidget(),
+            registerButton(),
+
+          ],
+                ),
+        ),)),
     );
   }
+  Widget titleWidget(){
+    return Text("Kwinstagram",
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+        fontSize: width!*0.08,
+      ),);
+  }
+  Widget registerButton(){
+    return MaterialButton(
+      height: height!*0.07,
+      minWidth: width!*0.7,
+      color: Colors.red,
+      onPressed: (){},
+    child: Text("Register",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: width!*0.059,
+        fontWeight: FontWeight.w500,
+      ),
+    ),);
+  }
+
 }
